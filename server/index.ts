ef3f023a907fba2d10747ce2f7d04ff49923285e
santhoshkumarsk14 <santhosh14.ravichandran@gmail.com>
2025-04-01 17:41:44 +0800

    Inital Commit

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from 'dotenv';
 
+dotenv.config();
+console.log('Postmark Token:', process.env.POSTMARK_SERVER_TOKEN);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
